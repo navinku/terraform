26112ff2d6344cd2c4d3100c7cfaf5c547a01cdc
Navin Kumar <nk_kumar@outlook.com>
2022-02-08 18:10:51 +0530

    myfolderanddocker

---
+++ myfiles/resources.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>3.0"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "deep-dive"
+  region  = "us-east-1"
+}
