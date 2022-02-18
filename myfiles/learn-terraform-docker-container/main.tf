cef2bd0a72d1607ef7c9a7d1506f1068c9a09e17
Navin Kumar <nk_kumar@outlook.com>
2022-02-18 10:35:36 +0530

    18Feb-WithVPC

--- myfiles/learn-terraform-docker-container/main.tf
+++ myfiles/learn-terraform-docker-container/main.tf
@@ -2,30 +2,21 @@ data "aws_ssm_parameter" "ami" {
   name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
 }
 resource "aws_instance" "app_server" {
-  ami                    = nonsensitive(data.aws_ssm_parameter.ami.value)
-  instance_type          = var.instance_type
-  vpc_security_group_ids = ["sg-02d65af4d09b6f2bb"]
-  subnet_id              = "subnet-084653feaa46384dd"
-
-  tags = {
-    Name = "ExampleAppServerInstance"
-  }
+  ami           = nonsensitive(data.aws_ssm_parameter.ami.value)
+  instance_type = var.instance_type
+  tags          = var.resource_tags
 }
 
 resource "aws_instance" "example_a" {
   ami           = data.aws_ssm_parameter.ami.value
   instance_type = var.instance_type
-  tags = {
-    Name = "example_a"
-  }
+  tags          = var.resource_tags
 }
 
 resource "aws_instance" "example_b" {
   ami           = data.aws_ssm_parameter.ami.value
   instance_type = var.instance_type
-  tags = {
-    Name = "example_b"
-  }
+  tags          = var.resource_tags
 }
 
 resource "aws_eip" "ip" {
@@ -40,23 +31,25 @@ resource "aws_s3_bucket" "example" {
 resource "aws_instance" "example_c" {
   ami           = data.aws_ssm_parameter.ami.value
   instance_type = var.instance_type
-  tags = {
-    Name = "example_c"
-  }
+  tags          = var.resource_tags
 
   depends_on = [aws_s3_bucket.example]
 }
 
-resource "aws_security_group" "sg_22" {
-  name   = "sg_22"
-  vpc_id = "vpc-08e523256674cb211"
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  name = "my-vpc"
+  #cidr = "10.0.0.0/16"
+  cidr = var.vpc_cidr_block
+  azs  = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  #private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  #public_subnets  = ["10.0.105.0/24", "10.0.106.0/24", "10.0.107.0/24"]
+  private_subnets    = slice(var.private_subnet_cidr_blocks, 0, var.private_subnet_count)
+  public_subnets     = slice(var.public_subnet_cidr_blocks, 0, var.public_subnet_count)
+  enable_nat_gateway = true
+  #enable_vpn_gateway = false
+  enable_vpn_gateway = var.enable_vpn_gateway
+
+  tags = var.resource_tags
 }
-
-
--- myfiles/learn-terraform-docker-container/variables.tf
+++ myfiles/learn-terraform-docker-container/variables.tf
@@ -13,7 +13,7 @@ variable "aws_region" {
 variable "vpc_cidr_block" {
   description = "CIDR block for VPC"
   type        = string
-  default     = "10.0.0.0/16"
+  default     = "100.0.0.0/16"
 }
 
 variable "instance_count" {
@@ -28,18 +28,24 @@ variable "enable_vpn_gateway" {
   default     = false
 }
 
+variable "public_subnet_count" {
+  description = "Number of public subnets."
+  type        = number
+  default     = 2
+}
+
 variable "public_subnet_cidr_blocks" {
   description = "Available cidr blocks for public subnets."
   type        = list(string)
   default = [
-    "10.0.1.0/24",
-    "10.0.2.0/24",
-    "10.0.3.0/24",
-    "10.0.4.0/24",
-    "10.0.5.0/24",
-    "10.0.6.0/24",
-    "10.0.7.0/24",
-    "10.0.8.0/24",
+    "100.0.1.0/24",
+    "100.0.2.0/24",
+    "100.0.3.0/24",
+    "100.0.4.0/24",
+    "100.0.5.0/24",
+    "100.0.6.0/24",
+    "100.0.7.0/24",
+    "100.0.8.0/24",
   ]
 }
 
@@ -53,14 +59,14 @@ variable "private_subnet_cidr_blocks" {
   description = "Available cidr blocks for private subnets."
   type        = list(string)
   default = [
-    "10.0.101.0/24",
-    "10.0.102.0/24",
-    "10.0.103.0/24",
-    "10.0.104.0/24",
-    "10.0.105.0/24",
-    "10.0.106.0/24",
-    "10.0.107.0/24",
-    "10.0.108.0/24",
+    "100.0.101.0/24",
+    "100.0.102.0/24",
+    "100.0.103.0/24",
+    "100.0.104.0/24",
+    "100.0.105.0/24",
+    "100.0.106.0/24",
+    "100.0.107.0/24",
+    "100.0.108.0/24",
   ]
 }
 
